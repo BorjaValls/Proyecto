a99a1e6e2ab6942768a03bd28d7ca261f385f8b1
Borja Valls <bvalls@iesfuentesanluis.org>
2022-04-24 11:50:10 +0200

    Creada clase Cliente

---
+++ Codigo/Cliente.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Cliente extends Persona{
+	
+	public Cliente (int telefono, String nombre, String dni, String direccion, String cuentaBancaria, String email, String sexo){
+		super(telefono, nombre, dni, direccion, cuentaBancaria, email, sexo);
+		
+		
+	}
+	
+	public String mostrarEmpleado(){
+		
+		String cadena = "Cliente: " +"\n" +toString() +"\n\n";
+		
+		return cadena;
+		
+	}
+	
+}
+
