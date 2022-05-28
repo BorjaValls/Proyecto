688a3084aa47553012e7e042bd856463c94c6ff1
Borja Valls <bvalls@iesfuentesanluis.org>
2022-05-28 17:47:23 +0200

    Java Doc ListaEmpleados

--- Codigo/ListaEmpleados.java
+++ Codigo/ListaEmpleados.java
@@ -1,24 +1,61 @@
 import java.util.*;
 import java.io.*;
 
+/**
+ * Clase ListaEmpleados
+ *
+ * Contiene los métodos y las operaciones para la administrar el array de Empleados
+ * que utiliza el programa. Entre las operaciones que realiza se encuentran las de añadir,
+ * buscar y eliminar por código de reserva, mostrar la lista completa de reservas, leer y cargar
+ * en fichero los datos del array y generar un documento html.
+ *
+ * @author Borja Valls García
+ * @version 1.1
+ */
+
 public class ListaEmpleados {
-	
+
+	/**
+	 * Array (o lista) donde se almacenan los empleados
+	 */
 	private ArrayList <Empleado> empleado;
+
+	/**
+	 * Carpeta global donde se guardan los ficheros
+	 */
 	private static final String RUTA = "./Ficheros/";
+
+	/**
+	 * Fichero donde se guardan y se leen los datos
+	 */
 	private static final String fempleados = "fempleados.txt";
-	
+
+	/**
+	 * Inicializa el objeto y todos los elementos asociados.
+	 */
 	public ListaEmpleados (){
 		
 		empleado = new ArrayList ();
 		
 	}
-	
+
+	/**
+	 * Guarda el empleado introducido en el array
+	 * @param x (Empleado a añadir)
+	 *
+	 */
 	public void anyadir (Empleado x){
 		
 		empleado.add(x);
 		
 	}
-	
+
+	/**
+	 * Busca coincidencias en el array mediante la introducción de un nombre
+	 * @param nombre (Nombre a buscar)
+	 * @return (Devuelve el empleado en caso de que lo encuentre y null en el caso contrario)
+	 *
+	 */
 	public Empleado buscarNombre (String nombre){
 		
 		for (int i = 0; i < empleado.size(); i++){
@@ -34,7 +71,13 @@ public class ListaEmpleados {
 		return null;
 		
 	}
-	
+
+	/**
+	 * Busca coincidencias en el array mediante la introducción de un DNI
+	 * @param dni (DNI a buscar)
+	 * @return (Devuelve el empleado en caso de que lo encuentre y null en el caso contrario)
+	 *
+	 */
 	public Empleado buscarDni (String dni){
 		
 		for (int i = 0; i < empleado.size(); i++){
@@ -51,7 +94,11 @@ public class ListaEmpleados {
 		
 	}
 	
-	/*public Empleado buscarID (int id){
+	/*
+	/**
+	 * @deprecated
+	 *
+	public Empleado buscarID (int id){
 		
 		for (int i = 0; i < empleado.size(); i++){
 			
@@ -92,7 +139,14 @@ public class ListaEmpleados {
 			return true;
 		
 	}*/
-	
+
+	/**
+	 * Busca coincidencias y elimina en el array mediante la introducción de un nombre
+	 * @param nombre (Nombre a buscar)
+	 * @return (Devuelve true si existe el empleado con el nombre introducido y lo elimina,
+	 * false si no lo encuentra)
+	 *
+	 */
 	public boolean eliminarNombre (String nombre){
 		
 		int posicion = -1;
@@ -118,7 +172,14 @@ public class ListaEmpleados {
 			return true;
 		
 	}
-	
+
+	/**
+	 * Busca coincidencias y elimina en el array mediante la introducción de un DNI
+	 * @param dni (DNI a buscar)
+	 * @return (Devuelve true si existe el empleado con el código introducido y lo elimina,
+	 * false si no lo encuentra)
+	 *
+	 */
 	public boolean eliminarDni (String dni){
 		
 		int posicion = -1;
@@ -144,7 +205,11 @@ public class ListaEmpleados {
 			return true;
 		
 	}
-	
+
+	/**
+	 * @return (Devuelve una cadena con los datos del objeto)
+	 *
+	 */
 	public String mostrarLE(){
 		
 		String cadena = "";
@@ -158,7 +223,10 @@ public class ListaEmpleados {
 		return cadena;
 		
 	}
-	
+
+	/**
+	 * Vuelca el contenido del array en el fichero
+	 */
 	public void guardarEmpleados () throws Exception{
 		
 		//Empleado empleado = new Empleado (autorizacion, puesto, telefono, nombre, dni, direccion, cuentaBancaria, email, sexo);
@@ -198,7 +266,10 @@ public class ListaEmpleados {
 		}
 		
 	}
-	
+
+	/**
+	 * Vuelca el contenido del fichero en el array
+	 */
 	public void leerEmpleados () throws Exception{
 		
 		//Empleado empleado = new Empleado (autorizacion, puesto, telefono, nombre, dni, direccion, cuentaBancaria, email, sexo);
